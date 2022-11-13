873a9d2a1ce5a3d63e0a2cd5f7a44dc2196f7027
G Haridev <haridevgireesh@gmail.com>
2022-11-14 00:33:32 +0530

    Added form validation.

--- core/forms.py
+++ core/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from core.validators import gmail_validation
+from django.core.exceptions import ValidationError
 
 class ContactForm(forms.Form):
     name = forms.CharField(
         label="Full name",
         max_length=50,
+        required=False,
     )
     email = forms.EmailField(
         max_length=100,
@@ -16,3 +18,17 @@ class ContactForm(forms.Form):
             attrs={"rows": 7}
         )
     )
+
+    def clean(self):
+        """
+        Raise `ValidationError` if user didn't provide both name and email.
+        """
+        cleaned_data = super().clean() # Making sure default cleaning is being done.
+        name = cleaned_data.get("name")
+        email = cleaned_data.get("email")
+
+        if not name and not email:
+            raise ValidationError("Please provide name or email.")
+        
+        return cleaned_data
+
